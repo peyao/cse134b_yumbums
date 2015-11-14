b7eb7207015bc4ec58079b2366fa1d7fd3a6c083
Peter Yao <peyao@ucsd.edu>
2015-11-14 04:31:42 -0800

    Add spinning preloader for AJAX calls.

--- hw4/js/vv-firebase.js
+++ hw4/js/vv-firebase.js
@@ -5,23 +5,26 @@ var PAGINATION_VALUE = 3;
 // Firebase
 var $firebase = {
     setUser: function(callback) {
+        $loader.show();
+
         var userId = localStorage.getItem('userId');
 
         if (userId === null) {
             var usersRef = firebaseRef.child('users');
-            var pushRef = usersRef.push({ created: Date.now() });
+            var pushRef = usersRef.push({
+                created: Date.now()
+            });
             userId = pushRef.key();
             localStorage.setItem('userId', userId);
 
             console.log('userId created: ' + userId);
         }
-        else {
-            //console.log('userId found: ' + userId);
-        }
 
+        $loader.hide();
         return callback(userId);
     },
     addHabit: function(habit, callback) {
+        $loader.show();
         this.setUser(function(userId) {
             var habitsRef = firebaseRef.child('users/' + userId + '/habits');
 
@@ -30,70 +33,118 @@ var $firebase = {
 
             return pushRef.key();
         });
+        $loader.hide();
     },
     getHabit: function(habitKey, callback) {
+        $loader.show();
         this.setUser(function(userId) {
             var habitRef = firebaseRef.child('users/' + userId + '/habits/' + habitKey);
             habitRef.once('value', function(snapshot) {
-                return callback(snapshot.val());
-            }, function (err) {
+                callback(snapshot.val());
+            }, function(err) {
                 console.log("Err: " + err);
             });
         });
+        $loader.hide();
     },
     getAllHabits: function(callback) {
+        $loader.show();
         this.setUser(function(userId) {
             var habitsRef = firebaseRef.child('users/' + userId + '/habits');
             habitsRef.orderByKey()
-                     .once('value', function(snapshot) {
-                         return callback(snapshot.val())
-                     }, function (err) {
-                         console.log('Err: ' + err);
-                     });
+                .once('value', function(snapshot) {
+                    callback(snapshot.val());
+                }, function(err) {
+                    console.log('Err: ' + err);
+                });
         });
+        $loader.hide();
     },
     getFirstHabits: function(callback) {
+        $loader.show();
         this.setUser(function(userId) {
             var habitsRef = firebaseRef.child('users/' + userId + '/habits');
             habitsRef.orderByKey()
-                     .limitToFirst(PAGINATION_VALUE)
-                     .once('value', function(snapshot) {
-                return callback(snapshot.val());
-            }, function (err) {
-                console.log("Err: " + err);
-            });
+                .limitToFirst(PAGINATION_VALUE)
+                .once('value', function(snapshot) {
+                    callback(snapshot.val());
+                }, function(err) {
+                    console.log("Err: " + err);
+                });
         });
+        $loader.hide();
     },
     getNextHabits: function(callback, habitKey) {
+        $loader.show();
         this.setUser(function(userId) {
             var habitsRef = firebaseRef.child('users/' + userId + '/habits');
             habitsRef.orderByKey()
-                     .startAt(habitKey)
-                     .limitToFirst(PAGINATION_VALUE + 1)
-                     .once('value', function(snapshot) {
-                return callback(snapshot.val());
-            }, function (err) {
-                console.log("Err: " + err);  
-            });
-        });  
+                .startAt(habitKey)
+                .limitToFirst(PAGINATION_VALUE + 1)
+                .once('value', function(snapshot) {
+                    callback(snapshot.val());
+                }, function(err) {
+                    console.log("Err: " + err);
+                });
+        });
+        $loader.hide();
     },
     updateHabit: function(habit, habitKey, callback) {
+        $loader.show();
         this.setUser(function(userId) {
             var habitRef = firebaseRef.child('users/' + userId + '/habits/' + habitKey);
             habitRef.set(habit, callback);
         });
+        $loader.hide();
     },
     deleteHabit: function(habitKey, callback) {
+        $loader.show();
         this.setUser(function(userId) {
             console.log('deleteHabit():habitKey: ' + habitKey);
             var habitRef = firebaseRef.child('users/' + userId + '/habits/' + habitKey);
             habitRef.set(null, callback);
         });
+        $loader.hide();
     }
 };
 
+var $loader = {
+    show: function() {
+        loader = document.getElementById('loader');
+        if (!loader)
+            return;
+        loader.style.visibility = 'visible';
+    },
+    hide: function() {
+        loader = document.getElementById('loader');
+        if (!loader)
+            return;
+        loader.classList.add("anim-loader-fade-out");
+        prefixedEvent(loader, "AnimationEnd", function() {
+            loader.style.visibility = 'hidden';
+            loader.className = ""; // Remove all classes
+        });
+    }
+};
 
+/*
+function pageTransitionOut(location) {
+    document.body.classList.add("anim-slide-out-right");
+    prefixedEvent(document.body, "AnimationEnd", function() {
+        document.getElementById('anim-wrapper').style.display = 'none';
+        window.location.href = location;
+    });
+}
+*/
 
+var pfx = ["webkit", "moz", "MS", "o", ""];
+function prefixedEvent(element, type, callback) {
+    for (var p = 0; p < pfx.length; p++) {
+        if (!pfx[p])
+            type = type.toLowerCase();
+        element.addEventListener(pfx[p] + type, callback, false);
+    }
+}
 
 /*
 firebase.child("hello").on("value", function(snapshot) {
