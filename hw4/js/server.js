3dbeb5864645906023eafdf175eb0a80b11e632e
zarni wang <zarnimng@gmail.com>
2015-11-18 11:53:53 -0800

    adds onesignal config for hosting and sets env to production

--- hw4/js/server.js
+++ hw4/js/server.js
@@ -8,20 +8,37 @@ function sendPushNotification(msg) {
     var req = new XMLHttpRequest();
     req.open('POST', 'https://onesignal.com/api/v1/notifications', true);
     req.setRequestHeader('Content-Type', 'application/json');
-    req.setRequestHeader('Authorization',
-        'Basic NjA0ZTdiMTUtN2RiNi00MzRkLThhNzUtMjFmNmE4ZGMwYjRj');
+
+    if(ENV == 'DEBUG') {
+        req.setRequestHeader('Authorization',
+            'Basic NjA0ZTdiMTUtN2RiNi00MzRkLThhNzUtMjFmNmE4ZGMwYjRj');
+    }
+    else {
+        req.setRequestHeader('Authorization',
+            'Basic M2Q1NTI0ZTctNDEyNy00Y2VjLWIzOWYtMjNmMDAwNjk5OTQ1');
+    }
 
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
             console.log(req.response);
         }
-    }
-
-    var message = {
-        app_id: '8f5de3fb-7578-4e0b-83e2-984979ad8b6d',
-        contents: {'en': msg},
-        include_player_ids: [oneSignalId]
     };
+
+    var message = {};
+    if(ENV == 'DEBUG') {
+        message = {
+            app_id: '8f5de3fb-7578-4e0b-83e2-984979ad8b6d',
+            contents: {'en': msg},
+            include_player_ids: [oneSignalId]
+        };
+    }
+    else {
+        message = {
+            app_id: '1f8dad4e-4d83-45ce-9a42-73434eee9182',
+            contents: {'en': msg},
+            include_player_ids: [oneSignalId]
+        };
+    }
 
     console.log('SENDING msg:');
     console.log(message);
--- hw4/vendors/onesignal.js
+++ hw4/vendors/onesignal.js
@@ -1,6 +1,8 @@
 var OneSignal = OneSignal || [];
 var oneSignalId = null;
 var isPushSupported = true;
+var ENV = 'PRODUCTION';
+//var ENV = 'DEBUG';
 
 initOneSignal();
 
@@ -8,11 +10,21 @@ function initOneSignal() {
     var DEFAULT_URL = 'http://localhost:8080/src/list.html';
 
     /* Configured for Chrome and Safari */
-    OneSignal.push(['init', {
-        appId: '8f5de3fb-7578-4e0b-83e2-984979ad8b6d',
-        safari_web_id: 'web.onesignal.auto.27be598e-7a22-4ed6-a01a-10378439b214',
-        subdomainName: 'yumbums'
-    }]);
+    if(ENV == 'DEBUG') {
+        OneSignal.push(['init', {
+            appId: '8f5de3fb-7578-4e0b-83e2-984979ad8b6d',
+            safari_web_id: 'web.onesignal.auto.27be598e-7a22-4ed6-a01a-10378439b214',
+            subdomainName: 'yumbums'
+        }]);
+    }
+    else if(ENV == 'PRODUCTION') {
+        OneSignal.push(['init', {
+            appId: '1f8dad4e-4d83-45ce-9a42-73434eee9182',
+            safari_web_id: 'web.onesignal.auto.1afb9025-a2b0-4a54-8c00-23b218b2b39b',
+            subdomainName: 'hostedyumbums'
+        }]);
+    }
+
     OneSignal.push(['setDefaultNotificationUrl', DEFAULT_URL]);
     OneSignal.push(['setDefaultTitle', 'This is a friendly reminder to complete your habits']);
 
