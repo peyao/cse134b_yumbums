ea81b42f1d79a001c829fda8238b5d73b50a8605
Jacob Keifer <jacobkeifer@MacBook-Pro-6.local>
2015-12-01 15:36:17 -0800

    Added minification, bundling by using gulp

---
+++ hw4/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
+
+gulp.task('default', function() {
+    return gulp.src('src/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist/src'));
+});
