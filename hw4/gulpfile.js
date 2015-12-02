ef5c0515bd41f3ce0fe53b91662cd606d223ada4
Jacob Keifer <jacobkeifer@MacBook-Pro-6.local>
2015-12-01 16:16:53 -0800

    added minification for html

--- hw4/gulpfile.js
+++ hw4/gulpfile.js
@@ -2,12 +2,24 @@ var gulp = require('gulp'),
     useref = require('gulp-useref'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
-    minifyCss = require('gulp-minify-css');
+    minifyCss = require('gulp-minify-css'),
+    minifyHTML = require('gulp-minify-html'),
+    runSequence = require('run-sequence');
 
-gulp.task('default', function() {
+gulp.task('default', function(){
+    runSequence('minifyExternals', 'minifySrc');
+});
+
+gulp.task('minifyExternals', function() {
     return gulp.src('src/*.html')
         .pipe(useref())
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCss()))
         .pipe(gulp.dest('dist/src'));
+});
+
+gulp.task('minifySrc', function(){
+    return gulp.src('dist/src/*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist/src'));
 });
